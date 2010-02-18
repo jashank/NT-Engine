5caa7ec7d7c5c8abc0f681f4c4ee1968547d2177
Alex Shipley <alexs91@vt.edu>
2010-02-18 04:28:05 +0000

    Started Button class.
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@57 5e24083e-1465-11df-9d2a-2365f6df44e6

---
+++ Kickle/Kickle/Button.h
@@ -0,0 +1,16 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <SFML/Graphics/Sprite.hpp>
+
+/********************************************************
+Button
+- Performs action when clicked by user.
+********************************************************/
+class Button : public sf::Sprite {
+ public:
+  /********************************************************
+  Constructors
+  - 
+  ********************************************************/
+  Button( 
