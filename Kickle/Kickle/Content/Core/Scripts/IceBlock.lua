5542b4eeb6102a0d09c185aba235c4230b975f7b
thanatos454 <thanatos454@gmail.com>
2010-04-20 09:32:34 +0000

    I started the long and arduous of scripting the proper tile placement for the block.  This will be one huge tree of if statements.  In order to facilitate testing I changed some of the level around.  Now it starts off with 6 ice blocks and the initial slimes are spawned in some walls so they can't move.
    Actually I think I've thought of a better way to do it but if anybody else has any ideas as well feel free to try your hand at it.  I need some sleep.  G'night
    
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@285 5e24083e-1465-11df-9d2a-2365f6df44e6

--- Kickle/Kickle/Content/Core/Scripts/IceBlock.lua
+++ Kickle/Kickle/Content/Core/Scripts/IceBlock.lua
@@ -12,7 +12,7 @@ function IceBlockTable.AILogic( IceBlock )
 		IceBlock:Move()
 
 		local facingX, facingY = GetTileObjectFaces( IceBlock )
-        tile = Level.GetTile( facingX, facingY );
+        local tile = Level.GetTile( facingX, facingY );
 
 		if ( tile == Tiles.WATER  or
              tile == Tiles.WATER_BOTTOM_LEFT or
@@ -21,9 +21,62 @@ function IceBlockTable.AILogic( IceBlock )
              tile == Tiles.ICE_CLIFF_LEFT or
              tile == Tiles.ICE_CLIFF_CENTER or
              tile == Tiles.ICE_CLIFF_RIGHT ) then
+
+            local dir = IceBlock:GetDir();
+
 			Level.DestroyGameObject( IceBlock )
-			Level.SetTile( facingX, facingY, ICE, 0 )
 			Level.CreateGameObject("Content/Core/Objects/Slime.xml", 11, 8 )
+
+
+-- [[
+            -- the tiles in cardinal directions from tile
+            -- n:north, s:south, e:east, w:west
+            local n = Level.GetTile( facingX, facingY-1 );
+            local s = Level.GetTile( facingX, facingY+1 );
+            local w = Level.GetTile( facingX-1, facingY );
+            local e = Level.GetTile( facingX+1, facingY );
+
+            -- the tiles in ordinal directions from tile
+            -- nw:northwest, ne:northeast, se:southeast, sw:southwest
+            local ne = Level.GetTile( facingX+1, facingY-1 );
+            local se = Level.GetTile( facingX+1, facingY+1 );
+            local nw = Level.GetTile( facingX-1, facingY-1 );
+            local sw = Level.GetTile( facingX-1, facingY+1 );
+
+
+
+            if( dir == UP ) then
+                Level.SetTile( facingX, facingY+1, Tiles.ICE, 0 );
+                if( tile == Tiles.ICE_CLIFF_CENTER ) then
+                    Level.SetTile( facingX, facingY, Tiles.ICE, 0 );
+                    Level.SetTile( facingX, facingY-1, Tiles.ICE, 0 );
+                elseif( tile == Tiles.ICE_CLIFF_LEFT ) then
+                    Level.SetTile( facingX, facingY, Tiles.ICE_LEFT, 0 );
+                    Level.SetTile( facingX, facingY-1, Tiles.ICE_LEFT, 0 );
+                elseif( tile == Tiles.ICE_CLIFF_RIGHT ) then
+                    Level.SetTile( facingX, facingY, Tiles.ICE_RIGHT, 0 );
+                    Level.SetTile( facingX, facingY-1, Tiles.ICE_RIGHT, 0 );
+                else
+                    Level.SetTile( facingX, facingY, Tiles.ICE_TOP, 0 );
+                end
+
+
+            elseif( dir == DOWN ) then
+                Level.SetTile( facingX, facingY-1, Tiles.ICE, 0 );
+                Level.SetTile( facingX, facingY, Tiles.ICE_BOTTOM, 0 );
+
+            elseif( dir == LEFT ) then
+                Level.SetTile( facingX-1, facingY, Tiles.ICE, 0 );
+                Level.SetTile( facingX, facingY, Tiles.ICE_LEFT, 0 );
+
+            elseif( dir == RIGHT ) then
+                Level.SetTile( facingX+1, facingY, Tiles.ICE, 0 );
+                Level.SetTile( facingX, facingY, Tiles.ICE_RIGHT, 0 );
+
+            end
+--]]
+
+
 		end
 	end
 end
