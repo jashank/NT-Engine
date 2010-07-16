200902933bd01f3b0af666a73c75eb12111d4e0b
bwright <bwright.au@gmail.com>
2010-07-16 10:35:01 +0000

    Added main.cpp documentation I forgot
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@438 5e24083e-1465-11df-9d2a-2365f6df44e6

--- Engine/Source Files/main.cpp
+++ Engine/Source Files/main.cpp
@@ -1,3 +1,13 @@
+/*! \mainpage NTEngine Index Page
+* \section intro_section Introduction
+* Welcome to the NTEngine API Documentation. NTEngine is a framework and 
+* engine for 2D tile based game development that relies heavily on 
+* metadata. Basically, you can make two completely different games without
+* ever recompiling or touching the source code. I guess you could say it's
+* a fancy parser. The core is implemented in C++, and the content is
+* written in XML and Lua. 
+*/
+
 #include <memory>
 #include "Utilities.h"
 #include <string>
