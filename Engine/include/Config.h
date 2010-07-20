5982e2448ed8599eba93459d127ddfef62caf93a
bwright <bwright.au@gmail.com>
2010-07-20 11:26:22 +0000

    Documented Config.h, I am doing multiple small documentation commits.
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@474 5e24083e-1465-11df-9d2a-2365f6df44e6

--- Engine/include/Config.h
+++ Engine/include/Config.h
@@ -1,21 +1,38 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/**
+ * Class Config. Defines some of the basic properties of the game such
+ * as the FPS rate and the screen size. This is being phased out in
+ * preference of a more generic system that is specified in XML.
+ */
+
 class Config {
  public:
+  /// Constructor
   Config();
+  /// Destructor
   ~Config();
 
   /// Loads Config data from Config file.
   static void Load();
 
+  /// Sets m_screenWidth to screenWidth
   static void SetScreenWidth( int screenWidth );
+  
+  /// Returns m_screenWidth
   static int GetScreenWidth();
-
+  
+  /// Sets m_screenHeight to screenHeight
   static void SetScreenHeight( int screenHeight );
+
+  /// Returns m_screenHeight
   static int GetScreenHeight();
 
+  /// Sets m_FPS to FPS
   static void SetFPS( int FPS );
+
+  /// Returns m_FPS
   static int GetFPS();
 
 private:
