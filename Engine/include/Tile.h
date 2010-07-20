25fc6145a69064eb0d436af1c0871c18e52c366b
bwright <bwright.au@gmail.com>
2010-07-20 09:55:02 +0000

    Added documentation for Tile.h
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@468 5e24083e-1465-11df-9d2a-2365f6df44e6

--- Engine/include/Tile.h
+++ Engine/include/Tile.h
@@ -3,10 +3,20 @@
 
 #include <string>
 
+/**
+ * Tile class. A base definition of a tile which consists of type,
+ * name, tile id (id) and collision id (cid). We may be moving this
+ * to a structure in the future to optimize preformance and since
+ * it does not really fit the class type.
+ */
+
 class Tile {
  public:
+  /// Constructor
   Tile();
+  /// Constructor with member definitions
   Tile( std::string type, std::string name, int id, int cid );
+  /// Destructors
   ~Tile();
 
   // Data
