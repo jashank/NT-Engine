9ff1e19a85f22e152e24094588c46fe861a31cd1
bwright <bwright.au@gmail.com>
2010-07-21 06:20:42 +0000

    Commited from the src/ instead of ../ so I forgot the camera.h file
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@481 5e24083e-1465-11df-9d2a-2365f6df44e6

---
+++ Engine/include/Camera.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "Rect.h"
+
+namespace nt {
+namespace scene {
+
+class Camera {
+ public:
+  /// Constructor Empty.
+  Camera() {};
+  /// Destructor Empty.
+  ~Camera() {};
+ private:
+  /// TODO Implement.
+};
+
+} // namespace nt
+} // namespace scene
+
+#endif // CAMERA_H
+
