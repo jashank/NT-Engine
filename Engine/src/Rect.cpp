cfc9a815fc14d6e6f924ee176382e307d259bc31
bwright <bwright.au@gmail.com>
2010-07-21 06:10:59 +0000

    Added an nt namespace that will overarch everything to prevent library namespace conflicts. Only to Vector and Rect at this stage.
    git-svn-id: https://noobtoob-kickle.googlecode.com/svn/trunk@478 5e24083e-1465-11df-9d2a-2365f6df44e6

--- Engine/src/Rect.cpp
+++ Engine/src/Rect.cpp
@@ -1,5 +1,6 @@
 #include "Rect.h"
 
+namespace nt {
 namespace core {
 
 float Rect::GetWidth() const {
@@ -9,19 +10,19 @@ float Rect::GetWidth() const {
 float Rect::GetHeight() const {
   return (bottomRight.y - topLeft.y);
 }
-core::Vector<float> Rect::GetSize() const {
-  return core::Vector<float>( GetWidth(), GetHeight() );
+nt::core::Vector<float> Rect::GetSize() const {
+  return nt::core::Vector<float>( GetWidth(), GetHeight() );
 }
 
 float Rect::GetArea() const {
   return ( GetWidth()*GetHeight() );
 }
 
-core::Vector<float> Rect::GetCenter() const {
-  return core::Vector<float>(GetWidth()/2, GetHeight()/2);
+nt::core::Vector<float> Rect::GetCenter() const {
+  return nt::core::Vector<float>(GetWidth()/2, GetHeight()/2);
 }
 
-bool Rect::Contains( const core::Vector<float> pos ) const {
+bool Rect::Contains( const nt::core::Vector<float> pos ) const {
   return ( topLeft.x <= pos.x &&
            topLeft.y <= pos.y &&
            bottomRight.x >= pos.x &&
@@ -35,14 +36,14 @@ bool Rect::Intersects( const Rect &other ) const {
            bottomRight.y > other.topLeft.x );
 }
   
-void Rect::SetCenter( core::Vector<float> centerPosition ) {
+void Rect::SetCenter( nt::core::Vector<float> centerPosition ) {
   topLeft.x = centerPosition.x - GetWidth()/2;
   topLeft.y = centerPosition.y - GetHeight()/2;
   bottomRight.x = centerPosition.x + GetWidth()/2;
   bottomRight.y = centerPosition.y + GetHeight()/2;
 }
   
-void Rect::SetPosition( core::Vector<float> position ) {
+void Rect::SetPosition( nt::core::Vector<float> position ) {
   float width = GetWidth();
   float height = GetHeight();
   topLeft = position;
@@ -50,7 +51,7 @@ void Rect::SetPosition( core::Vector<float> position ) {
   bottomRight.y = topLeft.y + height;
 }
   
-void Rect::SetScale( core::Vector<float> scale ) {
+void Rect::SetScale( nt::core::Vector<float> scale ) {
   bottomRight.x = topLeft.x + scale.x;
   bottomRight.y = topLeft.y + scale.y;
 }
@@ -88,3 +89,4 @@ bool operator>=( const Rect &other ) const {
 */
 
 } // namespace core
+} // namespace nt
