48452b25407d784b9cb29f05a748dfc5ccd30685
Alex Shipley <alexs91@vt.edu>
2010-10-07 23:10:31 -0400

    Finished Matrix2D. Note to anyone reading this, iterators in C++ are just a concept, and are literally often just implemented as raw pointers. See this commit for what I mean.

--- Engine/include/Matrix2D.h
+++ Engine/include/Matrix2D.h
@@ -1,41 +1,69 @@
 #ifndef MATRIX2D_H
 #define MATRIX2D_H
 
+#include <vector>
+
 namespace nt {
 namespace core {
 
 /**
- * Lightweight 2D matrix with basic indicing and iteration operations.
- * Index via (column,row) for more convenient (x,y) access.
- * Does index checking.
+ * Simple 2D matrix with basic indicing and iteration operations.
+ * Index via (column,row) for more convenient (x,y) access. Starts at (0,0).
+ * Does not do index checking.
  */
 template < typename T >
 class Matrix2D {
  public:
+  typedef T* iterator;
+
   Matrix2D( unsigned int cols, unsigned int rows )
     :m_cols(cols), m_rows(rows) {
     m_mat = new T[cols * rows];
   }
 
   ~Matrix2D() {
-    delete m_mat;
+    delete[] m_mat;
   }
 
   /**
-   * Returns element located at (col, row). Returns NULL if indice is invalid.
+   * Returns pointer to element located at (col, row). 
+   * Does not check if indice is valid.
    */
-  T *Matrix::operator() ( unsigned col, unsigned row ) {
-    if ( col >= m_cols || row >= m_rows ) {
-      return NULL;
-    }
-    return m_mat[m_rows*col + row];
+  T *operator() ( unsigned int col, unsigned int row ) {
+    return m_mat[(m_col * row) + col];
+  }
 
+  /**
+   * Returns (0,0) element of matrix.
+   */
+  iterator begin() {
+    return &m_mat[0];
+  }
+
+  /**
+   * Returns one past last element of matrix.
+   */
+  iterator end() {
+    return &m_mat[m_cols * m_rows];
+  }
   
  private:
-  std::vector<std::vector<T> >> m_
-  unsigned int m_cols;
-  unsigned int m_rows;
-  T *m_mat;
-}
+  //@{
+  /**
+   * Restrict default constructor, copy constructor, and assignment.
+   */
+  Matrix2D();
+  Matrix2D( const Matrix2D &mat );
+  Matrix2D& operator=( const Matrix2D &mat );
+  //@}
+
+  unsigned int m_cols; /** Columns in matrix. */
+  unsigned int m_rows; /** Rows in matrix. */
+  T* m_mat; /** Array representation of matrix. */
+};
+
+} // namespace core
+} // namespace nt
 
 #undef // MATRIX2D_H
+
