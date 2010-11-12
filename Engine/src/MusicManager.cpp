76f900cf15915df9bff2759f5353aecb02fcb87c
Alex Shipley <alexs91@vt.edu>
2010-11-12 02:27:39 -0500

    Started writing MusicManager but can't focus, and don't want to work in that state. Will finish tomorrow (hopefully I'll think up a good solution to keeping music playing between state changes).

---
+++ Engine/src/MusicManager.cpp
@@ -0,0 +1,52 @@
+#include "MusicManager.h"
+
+#include <utility>
+
+#include <SFML/Audio/Music.hpp>
+
+#include "ResourceLib.h"
+#include "Utilities.h"
+
+/*****************************
+ * Constructor and Destructor
+ ****************************/
+MusicManager::MusicManager()
+  :m_music( NULL ) {}
+
+MusicManager::~MusicManager() {
+  m_music = NULL;
+}
+
+/*****************************
+ * Public Methods
+ ****************************/
+bool MusicManager::LoadData( const TiXmlElement *root ) {
+  const TiXmlElement *song = root->FirstChildElement( "song" );
+  if ( song ) {
+    do {
+      const char *name = song->Attribute( "name" );
+      const char *path = song->Attribute( "path" );
+
+      if ( path ) {
+        if ( nt::rsrc::LoadMusic( path )) {
+
+          std::pair<std::string, std::string> pair;
+          if ( name ) {
+            pair = std::make_pair( name, path );
+          } else {
+            pair = std::make_pair( path, path );
+          }
+          m_namePaths.insert( pair );
+        }
+      } else {
+        LogErr( "Path not specified for song element in State file." );
+      }
+
+    } while ( song = song->NextSiblingElement( "song" ));
+  }
+}
+
+
+int MusicManager::LuaPlayMusic( lua_State *L ) {
+  // If 
+}
