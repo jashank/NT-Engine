e1f0453ac0a14ed38596c4c2d594be494da68d19
Alex Shipley <alexs91@vt.edu>
2010-12-16 19:27:54 -0500

    Err, now actually finished Point. Realized that mathematically it doesn't really make sense to add 2 points together, subtract, etc. so instead just have operators to add/subtract/etc. a scalar value to the coordinates of a point.

--- Engine/include/Point.h
+++ Engine/include/Point.h
@@ -20,78 +20,52 @@ class Point {
    */
   Point<T> &operator=( const Point<T> &rhs ) {
     if ( this != &rhs ) {
-      x = rhs.x;
-      y = rhs.y;
-      z = rhs.z;
+      x = rhs.x; y = rhs.y; z = rhs.z;
     }
     return *this;
   }
 
-  /**
-   * Add/assign coordintes of rhs to this Point.
-   */
-  Point<T> &operator+=( const Point<T> &rhs ) {
-    if ( this != &rhs ) {
-      x += rhs.x;
-      y += rhs.y;
-      z += rhs.z;
-    }
-    return *this;
-  } 
-
-  /**
-   * Subtract/assign coordinates of rhs from this Point.
-   */
-  Point<T> &operator-=( const Point<T> &rhs ) {
-    if ( this != &rhs ) {
-      x -= rhs.x;
-      y -= rhs.y;
-      z -= rhs.z;
-    }
+  /** Adds val to each coordinate of this Point. */
+  Point<T> &operator+=( T val ) {
+    x += val; y += val; z += val;
     return *this;
   }
 
-  /**
-   * Multiply/assign coordinates of rhs by this Point.
-   */
-  Point<T> &operator*=( const Point<T> &rhs ) {
-    if ( this != &rhs ) {
-      x *= rhs.x;
-      y *= rhs.y;
-      z *= rhs.z;
-    }
+  /** Subtracts val from each coordinate of this Point. */
+  Point<T> &operator-=( T val ) {
+    x -= val; y -= val; z -= val;
     return *this;
   }
 
-  /**
-   * Divide/assign coordinates of rhs by this Point.
-   * Checks for divide by 0, if one is to occur then the division for that
-   * coordinate is not performed (but the others still are).
-   */
-  Point<T> &operator/=( const Point<T> &rhs ) {
-    if ( this != &rhs ) {
-      if ( rhs.x != 0 ) x /= rhs.x;
-      if ( rhs.y != 0 ) y /= rhs.y;
-      if ( rhs.z != 0 ) z /= rhs.z;
+  /** Multiplies val by each coordinate of this Point. */
+  Point<T> &operator*=( T val ) {
+    x *= val; y *= val; z *= val;
+    return *this;
+  }
+
+  /** Divides each coordinate by val, unless val = 0. */
+  Point<T> &operator/=( T val ) {
+    if ( val != 0 ) {
+      x /= val; y /= val; z /= val;
     }
     return *this;
   }
 
   // Return new Points.
-  const Point<T> operator+( const Point<T> &rhs ) const {
-    return Point<T>(*this) += rhs;
+  const Point<T> operator+( T val ) const {
+    return Point<T>(*this) += val;
   }
 
-  const Point<T> operator-( const Point<T> &rhs ) const {
-    return Point<T>(*this) -= rhs;
+  const Point<T> operator-( T val ) const {
+    return Point<T>(*this) -= val;
   }
 
-  const Point<T> operator*( const Point<T> &rhs ) const {
-    return Point<T>(*this) *= rhs;
+  const Point<T> operator*( T val ) const {
+    return Point<T>(*this) *= val;
   }
 
-  const Point<T> operator/( const Point<T> &rhs ) const {
-    return Point<T>(*this) /= rhs;
+  const Point<T> operator/( T val ) const {
+    return Point<T>(*this) /= val;
   }
 
   /**
